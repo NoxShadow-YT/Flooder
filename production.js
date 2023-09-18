e7b39929f2eac5202e7324b924affe5dbeeea1c4
诺影科技 NoxShadow Tech <115096818+HackerYijia@users.noreply.github.com>
2023-09-18 13:24:09 -0400

    Create production.js

---
+++ production.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+  var script = document.createElement('script');
+  script.src = 'https://raw.githubusercontent.com/NoxShadow-YT/Flooder/main/standalone.js';
+  document.body.appendChild(script);
+})();
